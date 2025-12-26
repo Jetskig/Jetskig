eccef8db36938a710e579ad1fbe46173234403f3
Jetskig <hokerekhakan@gmail.com>
2025-12-26 03:12:19 +0300

    f

---
+++ constants.ts
@@ -0,0 +1,15 @@
+export const EMAILJS_CONFIG = {
+  PUBLIC_KEY: 'sxp_7o7YkO2AvzqEy',
+  SERVICE_ID: 'service_z7a6d9k',
+  TEMPLATE_ID: 'template_p1jchub',
+};
+
+export const NAVIGATION_LINKS = [
+  { name: 'Oyunlar', href: '#games' },
+  { name: 'Hakkımda', href: '#about' },
+  { name: 'İletişim', href: '#contact' },
+];
+
+export const SOCIAL_LINKS = {
+  email: 'jetskigstudio@gmail.com',
+};
